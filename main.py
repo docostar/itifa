694a631f26090b493df92a97a595f88cbed798e5
Rahul <docostar@panel.rokan.online>
2024-12-24 23:12:16 +0530

    Roll_no wise excel file created

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ demo_file= "FA_Output.xlsx"
 # workbook object is created
 wb_input = load_workbook(input_file)
 
-
-
 sheet = wb_input["Instructor"]
 si_name = sheet["B1"].value
 year = sheet["B2"].value
@@ -24,44 +22,36 @@ duration = sheet["B6"].value
 location = "BAGASARA"
 
 sheet_name = wb_input["Name_Rollno"]
+student_start_row = 2
+student_end_row = sheet_name.max_row
 sheet_lo = wb_input["LO"]
 lo_no = sheet_name["G2"].value
 
 output_file="LO_"+str(lo_no)+".xlsx"
-
 shutil.copy(demo_file, output_file)
-
-print("File copied successfully.")
-
+print(f"File {output_file} created successfully.")
 wb_out = load_workbook(output_file)
-
 start_practical, end_practical = get_practical_number(sheet_lo,lo_no)
 
+for student_row_no in range(student_start_row,student_end_row+1):
+    student_row_str=str(student_row_no)
+    student_name = sheet_name["B"+student_row_str].value
+    Roll_No = sheet_name["A"+student_row_str].value
+    student_main_mark = sheet_name["C"+student_row_str].value
 
-student_row=3
+    sheet_to_copy = wb_out["demo"]
+    student_sheet = wb_out.copy_worksheet(sheet_to_copy)
+    student_sheet.title = str(Roll_No)
 
-student_row_str=str(student_row)
-student_name = sheet_name["B"+student_row_str].value
-Roll_No = sheet_name["A"+student_row_str].value
-student_main_mark = sheet_name["C"+student_row_str].value
-
-sheet_to_copy = wb_out["Sheet1"]
-student_sheet = wb_out.copy_worksheet(sheet_to_copy)
-student_sheet.title = str(Roll_No)
-
-student_sheet["E2"].value = student_name
-student_sheet["U2"].value = Roll_No
-student_sheet["AD2"].value = year
-student_sheet["AL2"].value = sem
-student_sheet["E3"].value = iti_name
-student_sheet["AD4"].value = location
-student_sheet["E5"].value = trade
-student_sheet["AD5"].value = duration
-student_sheet["AK5"].value = si_name
+    student_sheet["E2"].value = student_name
+    student_sheet["U2"].value = Roll_No
+    student_sheet["AD2"].value = year
+    student_sheet["AL2"].value = sem
+    student_sheet["E3"].value = iti_name
+    student_sheet["AL3"].value = batch
+    student_sheet["AD4"].value = location
+    student_sheet["E5"].value = trade
+    student_sheet["AD5"].value = duration
+    student_sheet["AK5"].value = si_name
 
 wb_out.save(output_file)
-
-
-
-
-
